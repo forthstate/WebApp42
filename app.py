5304361ac69ff28c418879160f3a77d2a672f0d6
forthstate <71984279+forthstate@users.noreply.github.com>
2022-09-25 20:35:26 +0000

    Remove unupdated app file

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect
-
-# Configure Flask application
-app = Flask(__name__)
-
-# Landing page
-@app.route("/")
-def index():
-	return render_template("index.html")
